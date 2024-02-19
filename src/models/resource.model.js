500f5d73fc295808fed6b3b2eb9fb7acdf27cdc2
Azad99-9 <shaikazad2003@gmail.com>
2024-02-20 00:30:57 +0530

    added ApiError ApiResponse utils

--- src/models/resource.model.js
+++ src/models/resource.model.js
@@ -1,23 +1,23 @@
 import mongoose, { Schema } from "mongoose";
 
 const resourceSchema = mongoose.Schema({
-    learning: {
-        type: Boolean,
-        required: true,
-    },
-    companyname: {
-        type: Schema.Types.ObjectId,
-        ref: "Company",
-        required: false,
-    },
-    applyLink: {
-        type: String,
-        required: true,
-    },
-    endDate: {
-        type: String,
-        required: true,
-    }
+  learning: {
+    type: Boolean,
+    required: true,
+  },
+  companyname: {
+    type: Schema.Types.ObjectId,
+    ref: "Company",
+    required: false,
+  },
+  applyLink: {
+    type: String,
+    required: true,
+  },
+  endDate: {
+    type: String,
+    required: true,
+  },
 });
 
 export const Resouce = mongoose.model("Resource", resourceSchema);
