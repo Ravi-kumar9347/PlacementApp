9da969766283a0039a31c60871d3fc43bb446bf5
Ravi-kumar9347 <raviroy4839@gmail.com>
2024-02-22 16:39:05 +0530

    companyUpdate model created

---
+++ src/models/companyUpdates.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const companyUpdatesSchema = new mongoose.Schema({
+  companyId: {
+    type: String,
+    required: true,
+  },
+  updates: [
+    {
+      updateMessage: {
+        type: String,
+        required: true,
+      },
+      images: [
+        {
+          type: String,
+        },
+      ],
+      links: [
+        {
+          type: String,
+        },
+      ],
+    },
+  ],
+});
+
+export const CompanyUpdates = mongoose.model(
+  "CompanyUpdates",
+  companyUpdatesSchema
+);
